d0ccaa607024ffdd61be8d098f6e2da0d669aa84
evakontos <ekontos1@binghamton.edu>
2021-07-16 15:02:01 -0400

    .

---
+++ js/script.js
@@ -0,0 +1,9 @@
+script.js
+
+const hamburger = document.getElementById('hamburger');
+const topnav-right = document.getElementById('topnav-right');
+
+hamburger.addEventListener('click,() => {
+	topnav-right.classList.toggle('show');
+	});
+
---
+++ script.js
@@ -0,0 +1,10 @@
+script.js
+
+<script>
+const hamburger = document.getElementById('hamburger');
+const topnav-right = document.getElementById('topnav-right');
+
+hamburger.addEventListener('click,() => {
+	topnav-right.classList.toggle('show');
+	});
+</script>
